753e616d50b53045fbda5bc5bf8cbb194197c19f
cheng <cheng.bai@live.com>
2025-04-25 22:05:21 -0700

    Viz world

--- src/world.py
+++ src/world.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import matplotlib
+
 from dataclasses import dataclass
 from src.action import Action
 from src.agent import Agent
@@ -30,5 +32,30 @@ class World:
 
         # reward rule: reward = (x2 - x1) - abs(y2 - y1)
 
-    def viz(self, agent: Agent):
-        pass
+        action_reward = action
+        reward = max(0, state.x + action)
+        return reward
+
+    def viz(self, ax: matplotlib.axes._axes.Axes, agent: Agent, config: Config):
+        assert ax is not None
+        assert agent is not None
+
+        x0 = agent.state.x
+        y0 = agent.state.y
+
+        xa = []
+        ya = []
+        for idx, action in enumerate(agent.action_history):
+            dx, dy = action.get_udpate()
+            xa.append(x0 + dx)
+            ya.append(y0 + dy)
+            x0 += dx
+            y0 += dy
+            ax.annotate(f"{idx}", xy=(x0, y0), xycoords="data", fontsize=12)
+        ax.plot(xa, ya, marker="o", linestyle="--", color="red")
+
+        # ax.set(
+        #     xlim=[config.world_min_x, config.world_max_x],
+        #     ylim=[config.world_min_y, config.world_max_y],
+        # )
+        ax.grid(which="major", axis="y")
